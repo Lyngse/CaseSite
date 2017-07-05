c3e2c8fcc890804a123ab6cda0989f7baff9930a
Lyngse <superlyng@msn.com>
2017-07-05 14:54:14 +0200

    A fuck ton

---
+++ ClientApp/app/how-it-works-business/how-it-works-business.component.ts
@@ -0,0 +1,12 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'how-it-works-business',
+    templateUrl: './how-it-works-business.component.html',
+    styleUrls: ['./how-it-works-business.component.css']
+})
+export class HowItWorksBusinessComponent {
+    constructor() {
+
+    }
+}
---
+++ ClientApp/app/how-it-works-students/how-it-works-students.component.ts
@@ -0,0 +1,12 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'how-it-works-students',
+    templateUrl: './how-it-works-students.component.html',
+    styleUrls: ['./how-it-works-students.component.css']
+})
+export class HowItWorksStudentsComponent {
+    constructor() {
+
+    }
+}
