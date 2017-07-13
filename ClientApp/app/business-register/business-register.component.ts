21e2bb8f903ce2f3ec7ad070192dd1eae093ebd3
Lyngse <superlyng@msn.com>
2017-07-13 09:12:23 +0200

    redirect

--- ClientApp/app/business-register/business-register.component.ts
+++ ClientApp/app/business-register/business-register.component.ts
@@ -69,6 +69,7 @@ export class BuisnessRegisterComponent {
                                 this.utilService.loading.next(false);
                                 this.utilService.alert.next({ type: "danger", titel: "Fejl", message: "Der skete en fejl ved upload af logo" });
                                 this.utilService.alert.next({ type: "success", titel: "Success", message: "Din virksomhed er blevet oprettet, prøv oprettelsen af logo under 'Ret oplysninger'." });
+                                this.router.navigate(['/login']);
                             });
                         }
                     }, err => {
